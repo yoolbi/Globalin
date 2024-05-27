f324e1c7c0176eb7fc372973003a997573df3d79
yoolbi <yoolbi716@gmail.com>
2024-05-27 18:19:58 +0900

    ERD

--- src/main/java/JdbcTester.java
+++ src/main/java/JdbcTester.java
@@ -1,9 +1,12 @@
+import java.util.UUID;
 
 public class JdbcTester {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		new Jdbc().testConnection();
+		// new Jdbc().testConnection();
+		UUID uuid = UUID.randomUUID();
+		System.out.println(uuid.toString());
 	}
 
 }
