87bdd13683cab36ffcba2ea48b4d696b4e911635
yoolbi <yoolbi716@gmail.com>
2024-05-31 16:37:59 +0900

    ch18: cursor, exception, ch19: procedure, function

--- src/main/java/JdbcTester.java
+++ src/main/java/JdbcTester.java
@@ -1,12 +1,11 @@
-import java.util.UUID;
-
 public class JdbcTester {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// new Jdbc().testConnection();
-		UUID uuid = UUID.randomUUID();
-		System.out.println(uuid.toString());
+		new Jdbc().testConnection(50);
+		// UUID uuid = UUID.randomUUID();
+		// System.out.println(uuid.toString());
 	}
 
 }
